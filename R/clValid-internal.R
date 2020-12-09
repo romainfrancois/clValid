229306a01e09411522c3a68b0cde8fd03f40093a
Biagio98 <biagio.incardona@outlook.it>
2020-12-09 11:59:52 +0100

    Update clValid-internal.R

--- R/clValid-internal.R
+++ R/clValid-internal.R
@@ -93,7 +93,7 @@ vClusters <- function(mat,clMethod,nClust,nclustMax, validation,
       co.del <- 0 ## for use in verbose printing of progress
       for (del in 1:ncol(mat)) {
         matDel <- mat[,-del]               ## matDel <- as.matrix(matDel)
-        DistDel <- as.dist(gower::gower.dist(matDel))
+        DistDel <- as.dist(gower:::gower.dist(matDel))
         switch(clMethod,
                hierarchical = clusterObjDel <- hclust(DistDel,method),
                kmeans = clusterObjInitDel <- hclust(DistDel,method),
